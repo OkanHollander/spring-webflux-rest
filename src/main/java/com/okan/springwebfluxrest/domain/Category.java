f7448e60cf4d1c2e800d4a34c4bbd114ae012918
Okan Hollander <o.hollander1990@gmail.com>
2020-01-09 20:10:52 +0100

    refactor Category

--- src/main/java/com/okan/springwebfluxrest/domain/Category.java
+++ src/main/java/com/okan/springwebfluxrest/domain/Category.java
@@ -1,6 +1,9 @@
 package com.okan.springwebfluxrest.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,7 +12,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
  * Date:     09/01/2020
  * Time:     20:00
  */
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Builder
 @Document
 public class Category {
 
